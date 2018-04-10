c68ba2dc373aba615c7980743cae74d82776af63
Rob Wood <rdrwood@gmail.com>
2018-04-10 09:16:48 -0700

    Adding the data types file from in class exercise 4/10/18

---
+++ DataTypes41018/DataTypeSample41018/DataTypeSample41018/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypeSample41018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte age = 30;
+            string name = "joe";
+            char firstInitial = "J";
+
+            short num = 5;
+            
+            // Dont use .NET types in C#
+            Byte age2 = 30;
+            String name2 = "joe";
+        }
+    }
+}
