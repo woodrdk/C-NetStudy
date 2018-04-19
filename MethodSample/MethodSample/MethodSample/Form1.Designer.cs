71566233287bf9b19aa7105959941d38524ec550
Rob Wood <rdrwood@gmail.com>
2018-04-19 11:03:00 -0700

    final end of class 4/19/18

--- MethodSample/MethodSample/MethodSample/Form1.Designer.cs
+++ MethodSample/MethodSample/MethodSample/Form1.Designer.cs
@@ -29,6 +29,17 @@
         private void InitializeComponent()
         {
             this.btnExit = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.calcsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblSubtotal = new System.Windows.Forms.Label();
+            this.lblTaxRate = new System.Windows.Forms.Label();
+            this.txtSubtotal = new System.Windows.Forms.TextBox();
+            this.txtTaxRate = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnExit
@@ -41,21 +52,125 @@
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.fontToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(833, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.calcsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // calcsToolStripMenuItem
+            // 
+            this.calcsToolStripMenuItem.Name = "calcsToolStripMenuItem";
+            this.calcsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.calcsToolStripMenuItem.Text = "Calcs";
+            this.calcsToolStripMenuItem.Click += new System.EventHandler(this.calcsToolStripMenuItem_Click);
+            // 
+            // fontToolStripMenuItem
+            // 
+            this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+            this.fontToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.fontToolStripMenuItem.Text = "Calculate";
+            this.fontToolStripMenuItem.Click += new System.EventHandler(this.fontToolStripMenuItem_Click);
+            // 
+            // lblSubtotal
+            // 
+            this.lblSubtotal.AutoSize = true;
+            this.lblSubtotal.Location = new System.Drawing.Point(124, 118);
+            this.lblSubtotal.Name = "lblSubtotal";
+            this.lblSubtotal.Size = new System.Drawing.Size(46, 13);
+            this.lblSubtotal.TabIndex = 2;
+            this.lblSubtotal.Text = "Subtotal";
+            // 
+            // lblTaxRate
+            // 
+            this.lblTaxRate.AutoSize = true;
+            this.lblTaxRate.Location = new System.Drawing.Point(263, 118);
+            this.lblTaxRate.Name = "lblTaxRate";
+            this.lblTaxRate.Size = new System.Drawing.Size(48, 13);
+            this.lblTaxRate.TabIndex = 3;
+            this.lblTaxRate.Text = "TaxRate";
+            // 
+            // txtSubtotal
+            // 
+            this.txtSubtotal.Location = new System.Drawing.Point(127, 149);
+            this.txtSubtotal.Name = "txtSubtotal";
+            this.txtSubtotal.Size = new System.Drawing.Size(100, 20);
+            this.txtSubtotal.TabIndex = 4;
+            // 
+            // txtTaxRate
+            // 
+            this.txtTaxRate.Location = new System.Drawing.Point(266, 149);
+            this.txtTaxRate.Name = "txtTaxRate";
+            this.txtTaxRate.Size = new System.Drawing.Size(100, 20);
+            this.txtTaxRate.TabIndex = 5;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(127, 217);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(239, 23);
+            this.btnCalc.TabIndex = 6;
+            this.btnCalc.Text = "Calculate Total";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.BtnCalc_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(833, 471);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtTaxRate);
+            this.Controls.Add(this.txtSubtotal);
+            this.Controls.Add(this.lblTaxRate);
+            this.Controls.Add(this.lblSubtotal);
             this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Invoice Total 2.0";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem calcsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fontToolStripMenuItem;
+        private System.Windows.Forms.Label lblSubtotal;
+        private System.Windows.Forms.Label lblTaxRate;
+        private System.Windows.Forms.TextBox txtSubtotal;
+        private System.Windows.Forms.TextBox txtTaxRate;
+        private System.Windows.Forms.Button btnCalc;
     }
 }
 
--- MethodSample/MethodSample/MethodSample/Form1.cs
+++ MethodSample/MethodSample/MethodSample/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MethodSample
@@ -19,7 +12,126 @@ namespace MethodSample
 
         private void BtnExit_Click(object sender, EventArgs e)
         {
+            ExitApplication();
+        }
 
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ExitApplication();
+
+        }
+
+        /// <summary>
+        /// The following method is used to close the application and checks if the user wants to close, 
+        /// if yes then the application closes.
+        /// </summary>
+
+        private void ExitApplication()
+        {
+            DialogResult result = MessageBox.Show("Are you sure? ", "Quit?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+
+        private void calcsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 secondForm = new Form2();
+            secondForm.Show();
+        }
+
+
+
+
+        
+        // delete following
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        //********************************************************************
+    
+        private void BtnCalc_Click(object sender, EventArgs e)
+        {
+            bool isDataValid = IsDataValid();
+
+
+            if (IsDataValid())
+            {
+                double subtotal = Convert.ToDouble(txtSubtotal.Text);
+                double taxRate = Convert.ToDouble(txtTaxRate.Text);
+
+                double invoiceTotal = CalculateTotal(subtotal, taxRate);
+                DisplayTotal(invoiceTotal);
+            }
+            
+            
+        }
+
+        private void DisplayTotal(object invoiceTotal)
+        {
+            MessageBox.Show($"Your total is {invoiceTotal:c}");
+        }
+
+        // calculates the math 
+        private double CalculateTotal(double subtotal, double taxRate)
+        {
+            return subtotal * (1 + taxRate);
+        }
+
+        private bool IsDataValid()
+        {
+            // get user input
+            string subtotalInput = txtSubtotal.Text;
+            string taxRateInput = txtTaxRate.Text;
+
+            bool isDataValid = false; // flag variable to check if data is true or false
+            // check input is valid
+            if (IsDouble(subtotalInput) && IsDouble(taxRateInput))
+            {
+                double subtotal = Convert.ToDouble(subtotalInput);
+                double taxRate = Convert.ToDouble(taxRateInput);
+
+                if (IsWithinRange(subtotal, 0.01, 500000) && IsWithinRange(taxRate, 0, 1))
+                {
+                    isDataValid = true;
+                }
+            }
+
+            return isDataValid;
+        }
+
+        // made this method to validate input is a number
+        private bool IsDouble(string input)
+        {
+            if(double.TryParse(input, out double test))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Checks if a value is within a specific range
+        /// </summary>
+        /// <param name="testNum">The number to be tested</param>
+        /// <param name="minValue">The inclusive minimum value</param>
+        /// <param name="maxValue">The inclusive maximum value</param>
+        /// <returns></returns>
+        private bool IsWithinRange(double testNum, double minValue, double maxValue)
+        {
+            if(testNum >= minValue && testNum <= maxValue)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
