67dc7e9437bcb850de3ca19ae4bce08194ca392b
Rob Wood <rdrwood@gmail.com>
2018-05-01 11:43:04 -0700

    exception handling

---
+++ ExceptionHandlingPractice/MoreExceptionHandling/MoreExceptionHandling/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace MoreExceptionHandling
+{
+    partial class frmMoreException
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(206, 49);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstName.TabIndex = 0;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(206, 117);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(100, 23);
+            this.btnSubmit.TabIndex = 1;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(103, 49);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(60, 13);
+            this.lblFirstName.TabIndex = 2;
+            this.lblFirstName.Text = "First Name:";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(103, 87);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(26, 13);
+            this.lblAge.TabIndex = 4;
+            this.lblAge.Text = "Age";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(206, 87);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 20);
+            this.txtAge.TabIndex = 3;
+            // 
+            // frmMoreException
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtFirstName);
+            this.Name = "frmMoreException";
+            this.Text = "More Exception Handling";
+            this.Load += new System.EventHandler(this.frmMoreException_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.TextBox txtAge;
+    }
+}
+
---
+++ ExceptionHandlingPractice/MoreExceptionHandling/MoreExceptionHandling/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MoreExceptionHandling
+{
+    public partial class frmMoreException : Form
+    {
+        public frmMoreException()
+        {
+            InitializeComponent();
+            error.BlinkStyle = ErrorBlinkStyle.NeverBlink;
+        }
+        ErrorProvider error = new ErrorProvider();
+
+        /// <summary>
+        /// Checks if textbox contains non white space character data
+        /// </summary>
+        /// <param name="box">text box to validate</param>
+        /// <returns>returns true if text box contains non white space character  data</returns>
+        private bool DoesTextBoxContainData(TextBox box)
+        {
+            if (string.IsNullOrWhiteSpace(box.Text))
+                return false;
+            return true;
+
+            /*
+             * 
+             * return box.Text != null & box.text.trim() != string.Empty;
+             * 
+             * if (box.Text == null || box.Text.Trim() == string.Empty)
+             * {
+             *     return false;
+             * }
+             * return true;
+             * 
+            */
+        }
+        /// <summary>
+        /// Checks if textbox contains character data & displays error if no data is present
+        /// </summary>
+        /// <param name="box">The textbox to validate</param>
+        /// <param name="nameOfTextBox">Human readable name of text box to display in error message</param>
+        /// <returns>Returns true if contains any character data</returns>
+        /// 
+        private bool DoesTextBoxContainData(TextBox box, string nameOfTextBox)
+        {
+            if (string.IsNullOrWhiteSpace(box.Text))
+            {
+                
+                error.SetError(txtFirstName, $"{nameOfTextBox} does not contain data");
+
+                // MessageBox.Show($"{nameOfTextBox} does not contain data");
+                return false;
+            }
+            return true;
+        }
+
+        private void frmMoreException_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            ResetTextBoxErrors();
+
+            //string allErrors = string.Empty;
+            //if(!DoesTextBoxContainData(txtFirstName))
+            //{
+            //    allErrors += "first name is required !\n";
+            //}
+            // check all validation 
+
+            // validate data
+            if (DoesTextBoxContainData(txtFirstName, "First Name") & DoesTextBoxContainInt32(txtAge, "Age"))
+            {
+                // add data to database 
+                MessageBox.Show("data added");
+            }
+        }
+
+        private void ResetTextBoxErrors()
+        {
+            //// remove error
+            //error.SetError(txtFirstName, null);
+            //error.SetError(txtAge, null);
+
+            //  advanced technique for clearing all errors
+            // finds all child controls of the form
+            foreach (Control c in this.Controls)
+            {
+                // check if control is a Textbox
+                if(c is TextBox)
+                {
+                    // clear the error on that textbox
+                    error.SetError(c, null);
+                }
+            }
+        }
+
+        private bool DoesTextBoxContainInt32(TextBox box, string name)
+        {
+            if (DoesTextBoxContainData(box))
+            {
+                try
+                {
+                    Convert.ToInt32(box.Text);
+                    return true;
+                }
+                catch
+                {
+                    error.SetError(txtAge, $"{name} must be a number!");
+                    return false;
+                }
+            }
+            error.SetError(txtAge, $"{name} is required");
+            return false;
+
+        }
+    }
+}
