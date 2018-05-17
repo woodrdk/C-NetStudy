8b38c5cbd9d178fb888f0146048e99f1827b1ea3
Rob Wood <rdrwood@gmail.com>
2018-05-17 13:27:05 -0700

    more work
    more

---
+++ AdvancedForms/AdvancedForms/AdvancedForms/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace AdvancedForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboCourseList = new System.Windows.Forms.ComboBox();
+            this.lstStudents = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshAllDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblCourseInfo = new System.Windows.Forms.Label();
+            this.lstExpirationYear = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cboCourseList
+            // 
+            this.cboCourseList.FormattingEnabled = true;
+            this.cboCourseList.Location = new System.Drawing.Point(518, 103);
+            this.cboCourseList.Name = "cboCourseList";
+            this.cboCourseList.Size = new System.Drawing.Size(121, 21);
+            this.cboCourseList.TabIndex = 0;
+            this.cboCourseList.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // lstStudents
+            // 
+            this.lstStudents.FormattingEnabled = true;
+            this.lstStudents.Location = new System.Drawing.Point(49, 103);
+            this.lstStudents.Name = "lstStudents";
+            this.lstStudents.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lstStudents.Size = new System.Drawing.Size(120, 95);
+            this.lstStudents.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dataToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dataToolStripMenuItem
+            // 
+            this.dataToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.refreshAllDataToolStripMenuItem});
+            this.dataToolStripMenuItem.Name = "dataToolStripMenuItem";
+            this.dataToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.dataToolStripMenuItem.Text = "Data";
+            // 
+            // refreshAllDataToolStripMenuItem
+            // 
+            this.refreshAllDataToolStripMenuItem.Name = "refreshAllDataToolStripMenuItem";
+            this.refreshAllDataToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.refreshAllDataToolStripMenuItem.Text = "Refresh All Data";
+            this.refreshAllDataToolStripMenuItem.Click += new System.EventHandler(this.refreshAllDataToolStripMenuItem_Click);
+            // 
+            // lblCourseInfo
+            // 
+            this.lblCourseInfo.Location = new System.Drawing.Point(186, 103);
+            this.lblCourseInfo.Name = "lblCourseInfo";
+            this.lblCourseInfo.Size = new System.Drawing.Size(294, 95);
+            this.lblCourseInfo.TabIndex = 3;
+            this.lblCourseInfo.Click += new System.EventHandler(this.lblCourseInfo_Click);
+            // 
+            // lstExpirationYear
+            // 
+            this.lstExpirationYear.FormattingEnabled = true;
+            this.lstExpirationYear.Location = new System.Drawing.Point(48, 265);
+            this.lstExpirationYear.Name = "lstExpirationYear";
+            this.lstExpirationYear.Size = new System.Drawing.Size(120, 95);
+            this.lstExpirationYear.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstExpirationYear);
+            this.Controls.Add(this.lblCourseInfo);
+            this.Controls.Add(this.lstStudents);
+            this.Controls.Add(this.cboCourseList);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboCourseList;
+        private System.Windows.Forms.ListBox lstStudents;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshAllDataToolStripMenuItem;
+        private System.Windows.Forms.Label lblCourseInfo;
+        private System.Windows.Forms.ListBox lstExpirationYear;
+    }
+}
+
---
+++ AdvancedForms/AdvancedForms/AdvancedForms/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdvancedForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // dont have any course selected
+            if (cboCourseList.SelectedIndex < 0)
+            {
+                // will empty it out
+                lblCourseInfo.Text = string.Empty;
+            }
+            else
+            {
+                string selCourse = cboCourseList.SelectedItem as string;
+
+                switch (selCourse)
+                {
+                    case "CPW 116":
+                        lblCourseInfo.Text = ".Net programming";
+                        break;
+                    case "CPW 203":
+                        lblCourseInfo.Text = "ADV Javascript";
+                        break;
+                    default:
+                        lblCourseInfo.Text = "No Data Available";
+                        break;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int currYear = DateTime.Today.Year;
+            int endYear = currYear + 4;
+            for (int years = currYear; years <= endYear; years++)
+            {
+                lstExpirationYear.Items.Add(years);
+            }
+
+            PopulateCourseList();
+            PopulateStudentList();
+        }
+
+        /// <summary>
+        /// Gets Students from database and populates them in the combobox
+        /// </summary>
+        private void PopulateStudentList()
+        {
+            lstStudents.Items.Clear();
+            // populate listbox
+            List<string> students = GetStudents();
+            students.Sort();
+            foreach (string stu in students)
+            {
+                lstStudents.Items.Add(stu);
+            }
+            if (cboCourseList.Items.Count >= 1)
+            {
+                cboCourseList.SelectedIndex = 0;
+            }
+        }
+
+        private void PopulateCourseList()
+        {
+            cboCourseList.Items.Clear();
+            cboCourseList.DropDownStyle = ComboBoxStyle.DropDownList;
+            List<string> courses = GetCourses();
+            courses.Sort();
+            foreach (string course in courses)
+            {
+                cboCourseList.Items.Add(course);
+            }
+        }
+
+        private List<string> GetStudents()
+        {
+            return new List<string>()
+            {
+                "John Doe", "Jane Doe", "Jim Jones"
+            };
+        }
+
+        private List<string> GetCourses()
+        {
+            /*
+            var courses = new List<string>();
+            courses.Add("CPW 116");
+            courses.Add("CPW 203");
+            return courses;
+            the prev 4 lines are the same as the next chunk of code
+             */
+
+            return new List<string>()
+            {
+                "CPW 116", "CPW 203", "CPW 143"
+            };
+        }
+
+        private void refreshAllDataToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PopulateCourseList();
+            PopulateStudentList();
+        }
+
+        private void lblCourseInfo_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
