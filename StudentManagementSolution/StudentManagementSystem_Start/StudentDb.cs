6a90e9cac8b18b633f9fd8e2f62d59e918ff393e
Rob Wood <rdrwood@gmail.com>
2018-05-31 10:55:00 -0700

    student management
    more work with the classes and making of classes

---
+++ StudentManagementSolution/StudentManagementSystem_Start/StudentDb.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem_Start
+{
+    static class StudentDb
+    {
+        public static List<Student> GetAllStudents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool DeleteStudent(string sid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool DeleteStudent(Student s)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool AddStudent(Student s)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool UpdateStudent(Student s)
+        {
+            throw new NotImplementedException();
+        }
+
+        // example of lambda usage
+        //public static double Multiply(int a) { return a * 2; }
+        //public static double Multiply(int a) => a * 2;
+    }
+}
